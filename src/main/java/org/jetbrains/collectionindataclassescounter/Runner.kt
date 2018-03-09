0897c4cdfbaea5e025bac799881a0408b4d3d3ed
Victor Petukhov <i@victor.am>
2018-03-09 11:42:15 +0300

    Collection types move to file

--- src/main/java/org/jetbrains/collectionindataclassescounter/Runner.kt
+++ src/main/java/org/jetbrains/collectionindataclassescounter/Runner.kt
@@ -1,129 +1,40 @@
 package org.jetbrains.collectionindataclassescounter
 
+import com.fasterxml.jackson.core.type.TypeReference
 import org.jetbrains.collectionindataclassescounter.helpers.TimeLogger
 import org.jetbrains.collectionindataclassescounter.io.DirectoryWalker
 import org.jetbrains.collectionindataclassescounter.io.FileWriter
+import org.jetbrains.collectionindataclassescounter.io.JsonFilesReader
 import org.jetbrains.collectionindataclassescounter.structures.CollectionsByDataClassesStatistic
 import java.util.regex.Pattern
 import java.util.regex.Pattern.DOTALL
 
 object Runner {
-    private val collectionIdentifiers = setOf(
-            "AbstractCollection",
-            "AbstractIterator",
-            "AbstractList",
-            "AbstractMap",
-            "AbstractMutableCollection",
-            "AbstractMutableList",
-            "AbstractMutableMap",
-            "AbstractMutableSet",
-            "AbstractSet",
-            "ArrayList",
-            "BooleanIterator",
-            "ByteIterator",
-            "CharIterator",
-            "Collection",
-            "DoubleIterator",
-            "FloatIterator",
-            "Grouping",
-            "HashMap",
-            "HashSet",
-            "IndexedValue",
-            "IntIterator",
-            "Iterable",
-            "Iterator",
-            "LinkedHashMap",
-            "LinkedHashSet",
-            "List",
-            "ListIterator",
-            "LongIterator",
-            "Map",
-            "MutableCollection",
-            "MutableIterable",
-            "MutableIterator",
-            "MutableList",
-            "MutableListIterator",
-            "MutableMap",
-            "MutableSet",
-            "RandomAccess",
-            "Set",
-            "ShortIterator",
-            "SortedSet",
-            "NavigableSet",
-            "Queue",
-            "BlockingQueue",
-            "TransferQueue",
-            "Deque",
-            "BlockingDeque",
-            "SortedMap",
-            "NavigableMap",
-            "ConcurrentMap",
-            "ConcurrentNavigableMap",
-            "BlockingQueue",
-            "TransferQueue",
-            "BlockingDeque",
-            "ConcurrentMap",
-            "ConcurrentNavigableMap",
-            "LinkedBlockingQueue",
-            "ArrayBlockingQueue",
-            "PriorityBlockingQueue",
-            "DelayQueue",
-            "SynchronousQueue",
-            "LinkedBlockingDeque",
-            "LinkedTransferQueue",
-            "CopyOnWriteArrayList",
-            "CopyOnWriteArraySet",
-            "ConcurrentSkipListSet",
-            "ConcurrentHashMap",
-            "ConcurrentSkipListMap",
-            "AbstractQueue",
-            "AbstractSequentialList",
-            "ArrayDeque",
-            "AttributeList",
-            "ConcurrentLinkedDeque",
-            "ConcurrentHashMap",
-            "ConcurrentLinkedQueue",
-            "ConcurrentSkipListSet",
-            "EnumSet",
-            "LinkedList",
-            "PriorityQueue",
-            "RoleList",
-            "RoleUnresolvedList",
-            "Stack",
-            "SynchronousQueue",
-            "TreeSet",
-            "Vector",
-            "Arrays",
-            "TreeMap",
-            "WeakHashMap",
-            "IdentityHashMap",
-            "Dictionary",
-            "Hashtable",
-            "Properties",
-            "BitSet",
-            "Enumeration",
-            "ConcurrentMap",
-            "Comparable",
-            "Comparator"
-    )
-
     private const val dataClassRegex = "data class .+?(?=\\()(?=((?:(?=.*?\\((?!.*?\\2)(.*\\)(?!.*\\3).*))(?=.*?\\)(?!.*?\\3)(.*)).)+?.*?(?=\\2)[^(]*(?=\\3\$)))"
     private const val multiLineCommentRegex = "/\\*.*\\*/"
     private const val singleLineCommentRegex = "//.*(?=\\n)"
     private const val stringsRegex = "/\".*\"/"
+    private const val multilineStringsRegex = "/\"\"\".*\"\"\"/"
     private const val dataClassFieldsRegex = "(?:val|var).+?:\\s*(?<collectionType>%s)"
+
+    private const val collectionTypesFilename = "./collectionTypes.json"
+    private const val resultFilename = "./result.json"
 
     fun run(sourcesDirectory: String) {
         val timeLogger = TimeLogger(task_name = "Collections in data class counting")
         val statistic = CollectionsByDataClassesStatistic()
+        val collectionTypesReference = object: TypeReference<ArrayList<String>>() {}
+        val collectionTypes = JsonFilesReader.readFile<ArrayList<String>>(collectionTypesFilename, collectionTypesReference)
 
         DirectoryWalker(sourcesDirectory).run {
             val timeLoggerFile = TimeLogger(task_name = "File $it processing")
+
             // get source code and remove comments and strings
             val sourceCode = it.readText()
                     .replace(multiLineCommentRegex.toRegex(RegexOption.DOT_MATCHES_ALL), "")
                     .replace(singleLineCommentRegex.toRegex(), "")
-                    .replace(stringsRegex.toRegex(RegexOption.DOT_MATCHES_ALL), "")
+                    .replace(multilineStringsRegex.toRegex(RegexOption.DOT_MATCHES_ALL), "")
+                    .replace(stringsRegex.toRegex(), "")
 
             // balancing group recursive parsing. See https://stackoverflow.com/a/47162099
             val patternClass = Pattern.compile(dataClassRegex, DOTALL)
@@ -131,7 +42,7 @@ object Runner {
 
             while (matcherClass.find()) {
                 val fields = matcherClass.group(1)
-                val collectionIdentifierGroups = collectionIdentifiers.joinToString("|")
+                val collectionIdentifierGroups = collectionTypes.joinToString("|")
                 val patternFields = Pattern.compile(dataClassFieldsRegex.format(collectionIdentifierGroups), DOTALL)
                 val matcherFields = patternFields.matcher(fields)
                 var isMarkClass = false
@@ -156,7 +67,7 @@ object Runner {
             timeLoggerFile.finish()
         }
 
-        FileWriter.write("./result.json", statistic)
+        FileWriter.write(resultFilename, statistic)
 
         timeLogger.finish(fullFinish = true)
     }
